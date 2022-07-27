5870b64edce65c7cbc49f619bf1ef1f51549f441
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-07-27 16:51:53 -0300

    nhost integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,31 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { myTheme } from "../shared/themes/Theme";
+import { ChakraProvider } from "@chakra-ui/react";
+import { NhostNextProvider, NhostClient } from "@nhost/nextjs";
+
 import { LayoutPageDefault } from "../shared/layout/LayoutPageDefault";
-import { TaskProvider } from "../shared/contexts/TaskContext";
-import { PomoProvider } from "../shared/contexts/PomoContext";
+import { TaskProvider, PomoProvider, UserProvider } from "../shared/contexts/";
+import { myTheme } from "../shared/themes/Theme";
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || "",
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || "",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={myTheme}>
-      <TaskProvider>
-        <PomoProvider>
-          <LayoutPageDefault>
-            <Component {...pageProps} />
-          </LayoutPageDefault>
-        </PomoProvider>
-      </TaskProvider>
-    </ChakraProvider>
+    <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
+      <UserProvider>
+        <ChakraProvider resetCSS theme={myTheme}>
+          <TaskProvider>
+            <PomoProvider>
+              <LayoutPageDefault>
+                <Component {...pageProps} />
+              </LayoutPageDefault>
+            </PomoProvider>
+          </TaskProvider>
+        </ChakraProvider>
+      </UserProvider>
+    </NhostNextProvider>
   );
 }
 
